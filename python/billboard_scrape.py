d0b70ed23b890afcd0825d8d90da6cca4fb7f29a
Miguel <mhernandez401405@gmail.com>
2025-07-25 03:59:15 +0000

    billboard scrape, youtube, and 1 data file

---
+++ python/billboard_scrape.py
@@ -0,0 +1,37 @@
+import billboard
+import json
+
+from datetime import date, timedelta
+
+
+# print(chart)
+def daterange(start_date: date, end_date: date):
+    for n in range(0, int((end_date - start_date).days), 7):
+        yield start_date + timedelta(n)
+
+def main():
+    START_YEAR = 1986
+    END_YEAR = 2025
+    for year in range(START_YEAR, END_YEAR):
+        start_dt = date(year, 1, 1)
+        end_dt = date(year, 12, 31)
+        year_songs = set()
+        for dt in daterange(start_dt, end_dt):
+            print(f'searching billboard chart for date {dt.strftime("%Y-%m-%d")}...')
+            try:
+                chart = billboard.ChartData('latin-songs', date=dt.strftime("%Y-%m-%d"))
+                for song in chart.entries:
+                    year_songs.add((song.title, song.artist))
+                print('done.')
+            except (billboard.BillboardNotFoundException, billboard.BillboardParseException) as e:
+                print(e)
+                continue
+        print('year songs size: ', len(year_songs))
+        year_songs_as_dicts = []
+        for t, a in year_songs:
+            year_songs_as_dicts.append({"title": t, "artist": a})
+        print(f'saving data to file {year}...')
+        with open(f'data/{year}.json', 'w') as f:
+            json.dump(year_songs_as_dicts, f)
+if __name__ == '__main__':
+    main()
---
+++ python/youtube.py
@@ -0,0 +1,115 @@
+import googleapiclient.discovery
+import os
+
+try:
+    YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
+except KeyError:
+    print('Youtube api key env variable not set')
+    exit()
+
+youtube = googleapiclient.discovery.build(serviceName='youtube', version='v3', developerKey=YOUTUBE_API_KEY)
+
+
+'''
+Search Results JSON
+https://developers.google.com/youtube/v3/docs/search#resource
+{
+    "kind": "youtube#searchResult",
+    "etag": etag,
+    "id": {
+        "kind": string,
+        "videoId": string,
+        "channelId": string,
+        "playlistId": string
+    },
+    "snippet": {
+        "publishedAt": datetime,
+        "channelId": string,
+        "title": string,
+        "description": string,
+        "thumbnails": {
+          (key): {
+              "url": string,
+              "width": unsigned integer,
+              "height": unsigned integer
+          }
+        },
+        "channelTitle": string,
+        "liveBroadcastContent": string
+    }
+}
+'''
+class SearchResult:
+    def __init__(self, search_result) -> None:
+        self.video_id = search_result['id']['videoId']
+        self.title = search_result['snippet']['title']
+        self.description = search_result['snippet']['description']
+        self.thumbnails = search_result['snippet']['thumbnails']['default']['url']
+
+'''
+Search Response JSON
+https://developers.google.com/youtube/v3/docs/videos/list#response
+{
+    "kind": "youtube#searchListResponse",
+    "etag": etag,
+    "nextPageToken": string,
+    "prevPageToken": string,
+    "regionCode": string,
+    "pageInfo": {
+        "totalResults": integer,
+        "resultsPerPage": integer
+    },
+    "items": [
+        search Resource
+    ]
+}
+'''
+class SearchResponse:
+    def __init__(self, search_response) -> None:
+        self.prev_page_token = search_response.get('prevPageToken')
+        self.next_page_token = search_response.get('nextPageToken')
+
+        # items contain list of videos
+        items = search_response.get('items')
+        self.search_results = []
+        for item in items:
+            search_result = SearchResult(item)
+            self.search_results.append(search_result)
+
+
+def search_yt(query, max_results=8, page_token=None):
+    # reference: https://developers.google.com/youtube/v3/docs/search/list
+    # reference: https://developers.google.com/youtube/v3/guides/implementation/pagination
+    request = youtube.search().list(
+        part="snippet", # search by keyword
+        maxResults=max_results,
+        pageToken=page_token, # optional, for going to next/prev result page
+        q=query,
+        type="video", # only include videos, not playlists/channels
+    )
+    response = request.execute()
+    search_response = SearchResponse(response)
+    return search_response
+
+def display_yt_results(search_response):
+    for search_result in search_response.search_results:
+        print(f'Video ID: {search_result.video_id}')
+        print(f'Title: {search_result.title}')
+        print()
+
+def get_video_view_count(id):
+    request = youtube.videos().list(
+        part="statistics",
+        id=id
+    )
+    response = request.execute()
+    view_count = response['items'][0]['statistics']['viewCount']
+    return view_count
+
+def main():
+    search_response = search_yt('lil peep nuts')
+    display_yt_results(search_response)
+    print(f'view count: {get_video_view_count(search_response.search_results[0].video_id)}')
+
+if __name__ == '__main__':
+    main()
