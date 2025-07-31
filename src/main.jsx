0815c2fd8f73a0588075abd991e2022f24327fe6
Miguel <mhernandez401405@gmail.com>
2025-07-31 22:51:26 +0000

    testing out manual google analytics page view events

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,46 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter, Routes, Route } from 'react-router'
 import LandingPage from './pages/LandingPage.jsx'
 import YearPage from './pages/YearPage.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <Routes>
-      <Route index path="/" element={
-        <StrictMode>
-          <LandingPage />
-        </StrictMode>
-      } />
-      <Route path="year">
-        <Route path=":year" element={
+var oldURL = window.location.href;
+
+function checkURLchange(){
+    if(window.location.href !== oldURL){
+        alert(`url changed! current URL: ${window.location.href} old URL: ${oldURL}`);
+        oldURL = window.location.href;
+        gtag("event", "page_view", {
+          page_path: window.location.pathname + window.location.search + window.location.hash,
+          page_search: location.search,
+          page_hash: location.hash
+        })
+    }
+}
+
+window.addEventListener('click', checkURLchange);
+// setInterval(checkURLchange, 3000);
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route index path="/" element={
           <StrictMode>
-            <YearPage />
+            <LandingPage />
           </StrictMode>
         } />
-      </Route>
-    </Routes>
-  </HashRouter>
-)
+        <Route path="year">
+          <Route path=":year" element={
+            <StrictMode>
+              <YearPage />
+            </StrictMode>
+          } />
+        </Route>
+      </Routes>
+    </HashRouter>
+  )
+}
+
+createRoot(document.getElementById('root')).render(<App />)
