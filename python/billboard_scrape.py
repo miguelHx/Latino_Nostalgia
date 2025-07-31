9bb58fd5601bbb5a7422f4c8a5b5d51f644a564e
Miguel <mhernandez401405@gmail.com>
2025-07-31 17:15:08 +0000

    prune function with test data

--- python/billboard_scrape.py
+++ python/billboard_scrape.py
@@ -9,7 +9,7 @@ def daterange(start_date: date, end_date: date):
     for n in range(0, int((end_date - start_date).days), 7):
         yield start_date + timedelta(n)
 
-def main():
+def billboard_scrape_latin_songs():
     START_YEAR = 2017
     END_YEAR = 2025
     for year in range(START_YEAR, END_YEAR):
@@ -33,5 +33,46 @@ def main():
         print(f'saving data to file {year}...')
         with open(f'data/{year}.json', 'w') as f:
             json.dump(year_songs_as_dicts, f)
+
+def prune_song_data():
+    # pruning december songs bleeding into january.
+    # do we want to prune from december of previous year OR 
+    # from january of the next year?
+    # thinking about it.
+    # could go either way, but what makes most sense?
+    # i don't want previous years songs bleeding into next year,
+    # so i think we'll remove songs from january instead of dec
+    # of previous year. Yes.
+    START_YEAR = 1986
+    END_YEAR = 1988
+    for year in range(START_YEAR, END_YEAR):
+        with open(f'test_data/{year}.json') as f:
+            songs_curr_year = json.load(f)
+            songs_next_year = []
+            print(f'year {year} songs size: ', len(songs_curr_year))
+            with open(f'test_data/{year+1}.json') as f2:
+                songs_next_year = json.load(f2)
+                print(f'year {year+1} songs size: ', len(songs_next_year))
+                for song_curr_year in songs_curr_year:
+                    # check if index exists in songs_next_year
+                    index = -1
+                    for i, song in enumerate(songs_next_year):
+                        if (song_curr_year['title'] == song['title'] and
+                            song_curr_year['artist'] == song['artist']):
+                            index = i
+                            break
+                    if index != -1:
+                        songs_next_year.pop(index)
+                print(f'songs next year after pruning size: ', len(songs_next_year))
+            # if songs_next_year:
+            #     with open(f'test_data/{year+1}.json', 'w') as f3:
+            #         json.dump(songs_next_year, f3)
+
+
+def main():
+    # billboard_scrape_latin_songs()
+    prune_song_data()
+
+
 if __name__ == '__main__':
     main()
