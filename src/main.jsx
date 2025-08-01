f6f005f0a3addf574de45360ae7f19527105caba
Miguel <mhernandez401405@gmail.com>
2025-08-01 22:22:20 +0000

    better way to do page view events, update params after seeing ga docs

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,46 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router'
+import { HashRouter, Routes, Route, useLocation } from 'react-router'
 import LandingPage from './pages/LandingPage.jsx'
 import YearPage from './pages/YearPage.jsx'
 import './index.css'
 
-var oldURL = window.location.href;
 
-function checkURLchange(){
-    if(window.location.href !== oldURL){
-        oldURL = window.location.href;
-        gtag("event", "page_view", {
-          page_path: window.location.pathname + window.location.search + window.location.hash,
-          page_search: location.search,
-          page_hash: location.hash
-        })
+function RoutesContainer() {
+  const location = useLocation()
+
+  useEffect(() => {
+    let url = window.location.href.split('#')[0]
+    if (url.endsWith('/')) {
+      url = url.slice(0, -1)
     }
-}
+    gtag("event", "page_view", {
+      page_location: url + location.pathname
+    })
+  }, [location])
 
-window.addEventListener('click', checkURLchange);
-// setInterval(checkURLchange, 3000);
+  return (
+    <Routes>
+      <Route index path="/" element={
+        <StrictMode>
+          <LandingPage />
+        </StrictMode>
+      } />
+      <Route path="year">
+        <Route path=":year" element={
+          <StrictMode>
+            <YearPage />
+          </StrictMode>
+        } />
+      </Route>
+    </Routes>
+  )
+}
 
 function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route index path="/" element={
-          <StrictMode>
-            <LandingPage />
-          </StrictMode>
-        } />
-        <Route path="year">
-          <Route path=":year" element={
-            <StrictMode>
-              <YearPage />
-            </StrictMode>
-          } />
-        </Route>
-      </Routes>
+      <RoutesContainer />
     </HashRouter>
   )
 }
