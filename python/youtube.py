c20d75f56874d226bc769c2a27fe34ba9b9457b4
Miguel <mhernandez401405@gmail.com>
2025-09-01 14:57:25 +0000

    next 100 songs

--- python/youtube.py
+++ python/youtube.py
@@ -108,8 +108,8 @@ def get_video_view_count(id):
     return view_count
 
 def get_youtube_links_from_songs():
-    START_YEAR = 2007
-    END_YEAR = 2007
+    START_YEAR = 2008
+    END_YEAR = 2008
     for year in range(START_YEAR, END_YEAR+1):
         songs = []
         file_path = f'data/{year}.json'
