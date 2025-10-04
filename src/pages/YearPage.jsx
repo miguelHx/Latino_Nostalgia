a4e5c400e1887aefcffc70cd3bd21eebcec0a965
Miguel <mhernandez401405@gmail.com>
2025-10-04 13:30:26 -0700

    new open graph image, fix small bug

--- src/pages/YearPage.jsx
+++ src/pages/YearPage.jsx
@@ -28,7 +28,7 @@ function YearPage() {
     const [currentVideo, setCurrentVideo] = useState(null);
     const playerRef = useRef(null);
     const [queue, setQueue] = useState([]);
-    const [doRenderQueueList, setDoRenderQueueList] = useState(false);
+    const [doRenderQueueList, setDoRenderQueueList] = useState(true);
     const queueRef = useRef([]);
     const { year } = useParams();
     const location = useLocation();
